f49158dad22a43b70bc0e6b67ae6516cee5b25d9
Mariana Svirzhevska <mariana_svirzhevska@epam.com>
2022-06-23 15:39:51 +0300

    HW-5.1 Setup webpack codespliting

---
+++ src/js/page2.js
@@ -0,0 +1,3 @@
+
+//This log should appear only on page 2
+console.log('It is page 2');
--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,63 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, 'src/js/index.js'),
-    output: {
-        filename: 'shared.bundle.js',
+    entry: {
+        page1: {
+            import: path.resolve(__dirname, 'src/js/page1.js'),
+            dependOn: 'shared',
+        },
+        page2: {
+            import: path.resolve(__dirname, 'src/js/page2.js'),
+            dependOn: 'shared',
+        },
+        shared: path.resolve(__dirname, 'src/js/index.js'),
     },
+    output: {
+        filename: '[name].bundle.js',
+    },
+
     module: {
         rules: [
             {
                 test: /\.html$/i,
-                loader: "html-loader",
+                loader: 'html-loader',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                        ['@babel/preset-env', {
+                            useBuiltIns: 'usage',
+                            targets: {
+                              esmodules: true
+                            }
+                          }]
+                        ]
+                    }
+                }
             },
         ],
     },
+    optimization: {
+        splitChunks: {
+          chunks: 'all',
+        },
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/page1.html",
-            filename: `page1.html`
+            entry: 'page1',
+            template: './src/page1.html',
+            filename: 'page1.html',
+            chunks: ['shared', 'page1'],
         }),
         new HtmlWebpackPlugin({
-            template: "./src/page2.html",
-            filename: `page2.html`
+            entry: 'page2',
+            template: './src/page2.html',
+            filename: 'page2.html',
+            chunks: ['shared', 'page2'],
         }),
     ],
 };
